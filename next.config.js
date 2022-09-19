d151e0d7b584deb89517acff7c689596b781a468
tai <nguyentientai10@gmail.com>
2022-09-19 09:58:23 +0700

    config env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  env: {
+    BASE_URL: "https://redis-orm-search.vercel.app",
+    NEXT_PUBLIC_REDIS_URL:
+      "redis://default:FsC8z6izntdBbhrY7dDOrkNGkrDqLq65@redis-17735.c91.us-east-1-3.ec2.cloud.redislabs.com:17735",
+    NEXT_PUBLIC_BASE_URL: "https://redis-orm-search.vercel.app",
+  },
+};
