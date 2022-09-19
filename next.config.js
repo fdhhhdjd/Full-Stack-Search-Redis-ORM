8467a009d55abfe9f50c0f7297904ff3bc89c9cf
tai <nguyentientai10@gmail.com>
2022-09-19 10:22:25 +0700

    fix env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   env: {
-    BASE_URL: "https://redis-orm-search.vercel.app",
-    NEXT_PUBLIC_REDIS_URL:
-      "redis://default:FsC8z6izntdBbhrY7dDOrkNGkrDqLq65@redis-17735.c91.us-east-1-3.ec2.cloud.redislabs.com:17735",
-    NEXT_PUBLIC_BASE_URL: "https://redis-orm-search.vercel.app",
+    BASE_URL: process.env.BASE_URL,
+    NEXT_PUBLIC_REDIS_URL: process.env.NEXT_PUBLIC_REDIS_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.BASE_URL,
   },
 };
